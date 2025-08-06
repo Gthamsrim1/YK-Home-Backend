7d3b3d6c21e6c2b51d715ae98a094a042eb76eed
Gthamsrim1 <106124044@nitt.edu>
2025-08-06 16:07:36 +0530

    ok

--- api/products/index.js
+++ api/products/index.js
@@ -21,10 +21,13 @@ export default async function handler(req, res) {
 
   // Enable CORS
   res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // your frontend domain
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS,PATCH');
-  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization'
+  );
+  
   if (req.method === 'OPTIONS') {
     res.status(200).end();
     return;
